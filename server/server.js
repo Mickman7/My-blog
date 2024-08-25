d6e812f219192eb22c3f84dc6a6eb12559645ed8
Mickman7 <michaeljohn_charles@yahoo.com>
2024-08-25 15:50:48 +0100

    changing path to port in server.js

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const path = 5000;
+const port = 5000;
 
 app.get('/api', (req, res) => {
   res.json({ users: ['user1', 'user2', 'user3', 'user4'] });
 });
 
 app.listen(path, () => {
-  console.log(`server started on path ${path}`);
+  console.log(`server started on port ${port}`);
 });
