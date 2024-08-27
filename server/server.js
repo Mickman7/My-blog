cdccdc749af1bff4377bbc590e72ba0e219e3588
Michael John-Charles <101274574+Mickman7@users.noreply.github.com>
2024-08-27 21:23:52 +0100

    login request testing

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+import 'dotenv/config'
+
 const express = require('express');
 const cors = require('corse');
 const mongoose = require('mongoose');
@@ -6,9 +8,16 @@ const port = 5000;
 
 const app = express();
 app.use(express.json())
-app.use(cors())
 
-mongoose.connect()
+mongoose.connect(process.env.DB_LOCATION,{
+  autoIndex: true,
+})
+
+app.post('/login', (req,res) => {
+  console.log(res.body);
+
+  res.json(req.body);
+})
 
 
 
