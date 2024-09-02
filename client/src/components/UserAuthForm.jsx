3a3a91f073567cacd8dc38a59012f525f2462357
Michael John-Charles <101274574+Mickman7@users.noreply.github.com>
2024-09-02 02:07:07 +0000

    changing signup functionality - unfinished

--- client/src/components/UserAuthForm.jsx
+++ client/src/components/UserAuthForm.jsx
@@ -1,64 +1,23 @@
 import React, { useRef, useState } from 'react'
 import axios from 'axios';
 import { NavLink } from 'react-router-dom';
+import { useSignup } from "../hooks/useSignup";
 
 
 const UserAuthForm = ({type})  =>{
     const [username,setUsername] = useState('');
     const [email,setEmail] = useState('');
     const [password,setPassword] = useState('');
+    const {signup, error, isLoading} = useSignup();
 
 
     const authForm = useRef();
 
-    const userAuthToServer = (serverRoute,formData) => {
-      axios.post(import.meta.env.VITE_SERVER_DOMAIN + serverRoute, formData)
-      .then(({data}) => {
-        console.log(data);
-      })
-      .catch(({response}) => {
-        console.error(response.data.error);
-      })
-    }
-
-    const handleSubmit = (e) => {
-      e.preventDefault();
-
-      let serverRoute = type == "sign-in" ? "/signin" : "/signup";
-
-      let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; // regex for email
-      let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/; // regex for password
-      
-      let form = new FormData(authForm.current);
-      let formData = {};
-
-      for(let [key, value] of form.entries()){
-        formData[key] = value;
-      }
-
-      // form validation
-      let {username, email, password} = formData;
-
-
-      if(!username || username.length < 3){
-        return console.log({"error": "username is invalid"});
-      }
-      if(!email.length){
-        return console.log({"error": "Enter an email"});
-      }
-      if(!emailRegex.test(email)){
-        return console.log({"error": "Email is invalid"});
-      }
-      if(!password || password.length < 6){
-        return console.log({"error": "Password must be more than 5 characters long"});
-      }
-      if(!passwordRegex.test(password)){
-        return console.log({"error": "Password should be 6 to 20 characters long with a numeric, 1 lowercase and 1 upercase letter"});
-      }
-
-      console.log("Form submitted successfully", formData);
-      userAuthToServer(serverRoute, formData);
-      // before finishing do the ui alert at timestamp 3:08:22
+    
+    const handleSubmit = async (e) => {
+      e.preventDefault()
+  
+      await signup(email, password)
     }
 
 
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -12,10 +12,10 @@ const loginUser = async (req, res) => {
 
 // signup a user
 const signupUser = async (req, res) => {
-  const {email, password} = req.body
+  const {username, email, password} = req.body
 
   try {
-    const user = await User.signup(email, password)
+    const user = await User.signup(username, email, password)
 
     // create a token
     // const token = createToken(user._id)
