b01979ce35e0d1523a9a7157f9db133401d1ceab
Michael <michaeljohn_charles@yahoo.com>
2024-09-06 20:58:41 +0100

    styling changes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {NavLink, Outlet} from 'react-router-dom'
 import { useAuthContext } from '../hooks/useAuthContext'
 import { useSignout } from '../hooks/useSignout'
+import blogLogo from '../images/blog-logo.jpg'
 
 
 
@@ -16,7 +17,7 @@ const Navbar = () => {
   return (
     <>
     <nav className='p-1 mb-5 border-b-2 flex items-center'>
-        <img className='w-12 h-12 mr-8' src="https://static.vecteezy.com/system/resources/previews/004/853/320/non_2x/book-read-library-study-line-icon-illustration-logo-template-suitable-for-many-purposes-free-vector.jpg" alt="nav logo" />
+        <img className='w-12 h-12 mr-8' src={blogLogo} alt="nav logo" />
         <div className='relative flex justify-around items-center gap-2'>
             <NavLink className='p-1 m-2 rounded hover:bg-gray-200' to="/">Home</NavLink>
             <NavLink className='p-1 m-2 rounded hover:bg-gray-200' to="/blogs">Blogs</NavLink>
