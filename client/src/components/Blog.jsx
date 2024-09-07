fac119d8af3905394324463a0b1a1f1536adacd1
Michael <michaeljohn_charles@yahoo.com>
2024-09-07 02:33:55 +0100

    minor routing change

--- client/src/components/Blog.jsx
+++ client/src/components/Blog.jsx
@@ -16,7 +16,7 @@ const Blog = ({blogs}) => {
         <h2 className='mx-2 font-bold text-start'>{blogs.title}</h2>
         <h4 className='mx-2 text-start' >{blogs.author}</h4>
         <p className='mx-2 my-2 font-semibold text-start'>{blogs.description}</p>
-        <NavLink to={user ? `/blogs/${blogs._id}` : '/signup'} className='m-2 text-sky-500 hover:underline underline-offset-4' type="submit">Read more</NavLink>
+        <NavLink to={user ? `/blogs/${blogs._id}` : '/signin'} className='m-2 text-sky-500 hover:underline underline-offset-4' type="submit">Read more</NavLink>
     </div>
   )
 }
